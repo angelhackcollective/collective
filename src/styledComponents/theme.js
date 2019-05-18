058471f4372d8912110e7f5d3625cde352b9972c
Andrew Locke <alocke12992@gmail.com>
2019-05-18 15:00:32 -0700

    Adds theme

---
+++ src/styledComponents/theme.js
@@ -0,0 +1,24 @@
+export default {
+  breakpoints: ['768px', '1040px', '1440px'],
+  fontSizes: [
+    12, 14, 16, 20, 24, 32, 48, 64
+  ],
+  colors: {
+    blue: '#031e46',
+    lightgray: '#979797',
+    white: '#ffffff',
+    logoRed: '#c72d4d',
+    red: '#a91735',
+  },
+  space: [
+    0, 4, 8, 16, 32, 64, 128, 256
+  ],
+  fonts: {
+    body: '"Lato", sans-serif',
+    header: '"Roboto", sans-serif',
+  },
+  shadows: {
+    small: '0 0 4px rgba(0, 0, 0, .125)',
+    large: '0 0 24px rgba(0, 0, 0, .125)'
+  }
+}
