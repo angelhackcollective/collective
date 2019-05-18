fc9c9b4fe0de0fbe62c89f1c394e522ed78d32b9
Andrew Locke <alocke12992@gmail.com>
2019-05-18 13:26:58 -0700

    init commit

---
+++ src/pages/Signup.js
@@ -0,0 +1,13 @@
+import import React, { Component } from 'react';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div>
+        Signup
+      </div>
+    );
+  }
+}
+
+export default Signup;
