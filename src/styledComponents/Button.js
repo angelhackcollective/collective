257504af41cb538d024d50360c9d3c280edc4ce9
Andrew Locke <alocke12992@gmail.com>
2019-05-18 22:06:38 -0700

    Button props

--- src/styledComponents/Button.js
+++ src/styledComponents/Button.js
@@ -6,11 +6,11 @@ const defaultButtonStyle = inverted => `
   font-size: 0.875rem;
   font-weight: bold;
   font-family: 'lato';
-  color: ${inverted ? colors.blue : colors.white};
-  background-color: ${inverted ? colors.white : colors.blue};
+  color: ${colors.white};
+  background-color: ${props => props.color ? colors[props.color] : colors.blue};
   border-radius: 2px;
   cursor: pointer;
-  transition: background-color .5s ease, color .5s ease, box-shadow 0.3s ease 0s, border-color .3s ease 0s;;
+  transition: background-color .5s ease, color .5s ease, box-shadow 0.3s ease 0s;
   padding-top: 12px;
   padding-bottom: 11px;
   padding-right: 28px;
@@ -23,12 +23,10 @@ const defaultButtonStyle = inverted => `
   }
 
   :hover {
-    background-color: ${inverted ? colors.blue : colors.white};
-    color: ${inverted ? colors.white : colors.blue};
-    box-shadow: 0px 0px 0px 2px ${inverted ? colors.white : colors.blue};
-    border-color: ${inverted ? colors.white : colors.blue};
-    transition: background-color .5s ease 0s, color .5s ease 0s, box-shadow 0.3s ease 0s, border-color .3s ease 0s;
-
+    background-color: ${colors.white};
+    color: ${props => props.color ? colors[props.color] : colors.blue};
+    box-shadow: 0px 0px 0px 2px ${props => props.color ? colors[props.color] : colors.blue};
+    transition: background-color .5s ease 0s, color .5s ease 0s, box-shadow 0.3s ease 0s;
   }
   :focus {
     outline: none;
