a1e609d1c68633a25569868a58bf7850ccf8328c
Andrew Locke <alocke12992@gmail.com>
2019-05-19 04:41:19 -0700

    Remove unneded files

--- src/components/AuthRoute/index.js
+++ src/components/AuthRoute/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import Section from '../../styledComponents/Section';
+import Container from '../../styledComponents/Container';
 
 const AuthRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={props =>
       localStorage.token ? (
-        <Component {...props} />
+        <Section dark>
+          <Container>
+            <Component {...props} />
+          </Container>
+        </Section>
       ) : (
         <Redirect
           to={{
