ee6d5d0f02ba6f948841bafb1acfe0333c46ba78
Andrew Locke <alocke12992@gmail.com>
2019-05-19 04:22:24 -0700

    Fin Splash image

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import splash from './background.png';
+import { withRouter } from 'react-router-dom';
 import {
   Wrapper,
+  Hero,
+  Btn,
 } from './styles';
 
-const Landing = () => {
+const Landing = ({history}) => {
   return (
     <Wrapper src={splash}>
-      <h1>HEy</h1>
+      <Hero>
+        <h1>Sick of unexpected side effects of birth control?</h1>
+        <p>Read experiences provided by women like you to find your best fit.</p>
+        <hr />
+        <Btn onClick={() => history.push("/signup")}>Start Now</Btn>
+      </Hero>
     </Wrapper>
   )
 };
 
-export default Landing;
+export default withRouter(Landing);
--- src/pages/Landing/styles.js
+++ src/pages/Landing/styles.js
@@ -1,13 +1,53 @@
 import styled from 'styled-components';
+import Button from '../../styledComponents/Button';
 
 const Wrapper = styled.div`
   position: relative;
-  padding-top: 145px;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   background-image: url('${props => props.src}');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: unset;
 `;
+
+const Hero = styled.div`
+  padding-top: 148px;
+  margin: 0 auto;
+  color: #ffffff;
+  text-align: center;
+  h1 {
+    font-family: Roboto;
+    font-size: 29px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin-top: 8px;
+    font-size: 18px;
+  }
+
+  hr {
+    border-bottom: solid 2px #ffffff;
+    width: 46px;
+    margin: 20px auto;
+  }
+`;
+
+const Btn = styled(Button)`
+  border-radius: 23.5px;
+  width: 200px;
+  max-width: unset;
+  height: 50px;
+  padding-top: 9px;
+  border: none;
+  font-size: 17px;
+`;
+
 
 export {
   Wrapper,
+  Hero,
+  Btn,
 };
